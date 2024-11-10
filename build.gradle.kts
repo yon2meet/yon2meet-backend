f6ce8f3e77d11c8ff09c2841eb2ff60826d78641
Hajin Kim <kimhajin@yonsei.ac.kr>
2024-11-10 17:28:13 +0900

    docs: apply Swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,8 @@ dependencies {
 
     implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
 
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
+
     implementation("io.jsonwebtoken:jjwt-api:0.12.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
